34b2b92edbdaa425076565e5ac405fa3f9bc360c
Alex <you@example.com>
2014-05-23 22:08:44 +0400

    readme

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var Neiron = require('./lib/neiron.js');
+var Link = require('./lib/link.js');
+var Probability = require('./lib/probability.js');
+
+module.exports = {
+	Neiron: Neiron,
+	Link: Link,
+	Probability: Probability
+};
