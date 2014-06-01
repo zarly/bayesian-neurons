017872e34db0d4913131237e11fca6c58470b98b
Alex <you@example.com>
2014-06-01 14:36:48 +0400

    start two layers

--- tests/two_layers.js
+++ tests/two_layers.js
@@ -2,7 +2,7 @@ require('should');
 var Neiron = require('../lib/neiron.js');
 	
 describe('logical verification tests', function(){
-	it('should solve AND task', function(){
+	/*it('should solve COR task', function(){
 		function studyCase (io) {
 			if (io[0]) enter1.signalTo(exit);
 			if (io[1]) enter2.signalTo(exit);
@@ -33,7 +33,7 @@ describe('logical verification tests', function(){
 		}
 		
 		var studuCases = [
-			[0,0,0], 
+			[0,0,1], 
 			[0,1,0], 
 			[1,0,0], 
 			[1,1,1]
@@ -52,12 +52,14 @@ describe('logical verification tests', function(){
 		checkCase(studuCases[1]);
 		checkCase(studuCases[2]);
 		checkCase(studuCases[3]);
-	});
+	});*/
 	
-	it('should solve OR task', function(){
+	it('should solve XOR task', function(){
 		function studyCase (io) {
-			if (io[0]) enter1.signalTo(exit);
-			if (io[1]) enter2.signalTo(exit);
+			if (io[0]) enter1.unconditionalSpike();
+			if (io[1]) enter2.unconditionalSpike();
+			neiron1.handle();
+			neiron2.handle();
 			
 			exit.feedback(!!io[2]);
 			
@@ -88,65 +90,45 @@ describe('logical verification tests', function(){
 			[0,0,0], 
 			[0,1,1], 
 			[1,0,1], 
-			[1,1,1]
-		];
-		var enter1 = new Neiron();
-		var enter2 = new Neiron();
-		var exit = new Neiron();
-		enter1.linkTo(exit);
-		enter2.linkTo(exit);
-		
-		for (var i=0; i < studuCases.length; i++) {
-			studyCase(studuCases[i]);
-		}
-		
-		checkCase(studuCases[0]);
-		checkCase(studuCases[1]);
-		checkCase(studuCases[2]);
-		checkCase(studuCases[3]);
-	});
-	
-	it('should solve TURNED AND task', function(){
-		function studyCase (io) {
-			if (io[0]) enter1.signalTo(exit);
-			if (io[1]) enter2.signalTo(exit);
-			
-			exit.feedback(!!io[2]);
-			
-			enter1.resetSignals();
-			enter2.resetSignals();
-			exit.resetSignals();
-		}
-		
-		function checkCase (io) {
-			exit.inputs[0].signal.should.be.false;
-			exit.inputs[1].signal.should.be.false;
-			
-			if (io[0]) enter1.signalTo(exit);
-			if (io[1]) enter2.signalTo(exit);
-			
-			if (!!io[2]) {
-				exit.isShouldSpike().should.be.true;
-			} else {
-				exit.isShouldSpike().should.be.false;
-			}
-			
-			enter1.resetSignals();
-			enter2.resetSignals();
-			exit.resetSignals();
-		}
-		
-		var studuCases = [
+			[1,1,0],
 			[0,0,0], 
 			[0,1,1], 
-			[1,0,0], 
+			[1,0,1], 
+			[1,1,0],
+			[0,0,0], 
+			[0,1,1], 
+			[1,0,1], 
+			[1,1,0],
+			[0,0,0], 
+			[0,1,1], 
+			[1,0,1], 
+			[1,1,0],
+			[0,0,0], 
+			[0,1,1], 
+			[1,0,1], 
 			[1,1,0]
 		];
 		var enter1 = new Neiron();
 		var enter2 = new Neiron();
+		var neiron1 = new Neiron();
+		var neiron2 = new Neiron();
 		var exit = new Neiron();
-		enter1.linkTo(exit);
-		enter2.linkTo(exit);
+		enter1.linkTo(neiron1);
+		enter1.linkTo(neiron2);
+		enter2.linkTo(neiron1);
+		enter2.linkTo(neiron2);
+		neiron1.linkTo(exit);
+		neiron2.linkTo(exit);
+		neiron1.spikeHistoricalProbability.incPos();
+		neiron2.spikeHistoricalProbability.incNeg();
+		/*enter1.outputs[0].conditionalProbability.incPos();
+		enter1.outputs[1].conditionalProbability.incNeg();
+		enter2.outputs[0].conditionalProbability.incNeg();
+		enter2.outputs[1].conditionalProbability.incPos();
+		enter1.outputs[0].unconditionalProbability.incPos();
+		enter1.outputs[1].unconditionalProbability.incNeg();
+		enter2.outputs[0].unconditionalProbability.incNeg();
+		enter2.outputs[1].unconditionalProbability.incPos();//*/
 		
 		for (var i=0; i < studuCases.length; i++) {
 			studyCase(studuCases[i]);
@@ -156,5 +138,5 @@ describe('logical verification tests', function(){
 		checkCase(studuCases[1]);
 		checkCase(studuCases[2]);
 		checkCase(studuCases[3]);
-	});
+	});//*/
 });
